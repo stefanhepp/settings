2c7ca1b4855e2709cd19e3fab3d3ddf1a9b88a60
Stefan Hepp <stefan@stefant.org>
2026-01-05 10:46:46 +0100

    Reorganized install script, new install targets

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ install_settings() {
     sudo usermod -s /bin/zsh `whoami`
     test -f ~/.vimrc || cp -av home/.vimrc* ~/
     test -f ~/.zshrc || cp -av home/.zshrc ~/
-    mkdir -p ~/Apps/Icons
-    cp -avf home/Apps/Icons/* ~/Apps/Icons
+    mkdir -p ~/Applications/Icons
+    cp -avf home/Applications/Icons/* ~/Applications/Icons
     mkdir -p ~/.ssh
     mkdir -p ~/tmp
     test -f ~/.ssh/config || cp -av home/.ssh/config ~/.ssh/
@@ -44,10 +44,33 @@ install_packages() {
     echo
     echo "*** Installing standard software ..."
     echo
-    sudo apt install -y vim zsh screen ksshaskpass vlc kdenlive gimp inkscape kdevelop clang clang-tidy cppcheck cmake cmake-gui git gitk kdiff3 net-tools curl scons
-    sudo apt install -y krusader krename arj rar unrar smb4k kde-config-flatpak
+    sudo apt install -y vim zsh screen ksshaskpass vlc gimp inkscape clang clang-tidy cppcheck cmake cmake-gui git gitk kdiff3 net-tools curl scons
+    sudo apt install -y krusader krename arj rar unrar smb4k
     sudo apt install -y pipx python3-pip python3-serial python3-numpy python3-scipy python3-opencv python3-tk python3-pil.imagetk python3-venv
-    sudo apt install -y ardour audacity lmms
+}
+
+install_repos() {
+    echo
+    echo "*** Installing flatpak repositories ..."
+    echo
+
+    sudo apt install -y flatpak
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+    sudo apt install -y kde-config-flatpak
+
+    sudo flatpak install -y \
+	com.github.tchx84.Flatseal
+
+    echo
+    echo "*** Install AppImageLauncher ..."
+    echo
+    
+    package=appimagelauncher_3.0.0-beta-2-gha287.96cb937_amd64.deb
+
+    wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v3.0.0-beta-3/$package -P /tmp
+    sudo apt install /tmp/$package
+    rm /tmp/$package
 }
 
 install_xtradebs() {
@@ -62,8 +85,8 @@ install_xtradebs() {
 	sudo apt update
     fi
 
-    # Using OpenRA from xtraDebs, since flatpak has lots of old dependencies
     sudo apt install -y openra
+    sudo apt install -y openrct2 openttd
 }
 
 install_mozilla_flatpak() {
@@ -89,15 +112,6 @@ install_mozilla_flatpak() {
     flatpak override --user org.mozilla.firefox --talk-name=org.freedesktop.ScreenSaver
 }
 
-install_kicad() {
-    echo
-    echo "*** Installing KiCAD ..."
-    echo
-    sudo add-apt-repository --yes ppa:kicad/kicad-9.0-releases
-    sudo apt update
-    sudo apt install -y --install-recommends kicad
-}
-
 install_touchscreen() {
     echo
     echo "*** Installing virtual keyboard ..."
@@ -109,35 +123,13 @@ install_touchscreen() {
 
 install_apps() {
     echo
-    echo "*** Installing snaps and flatpaks ..."
+    echo "*** Installing flatpaks ..."
     echo
-    sudo apt install -y flatpak
-    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
     sudo flatpak install -y \
-        org.freecad.FreeCAD \
-	org.musescore.MuseScore \
         com.xnview.XnViewMP \
         com.jgraph.drawio.desktop \
-	com.jetbrains.PyCharm-Professional \
-        com.prusa3d.PrusaSlicer \
-        com.valvesoftware.Steam \
-        com.valvesoftware.SteamLink \
-	com.heroicgameslauncher.hgl \
-	com.usebottles.bottles \
-        com.obsproject.Studio \
-        org.jdownloader.JDownloader \
-	org.blender.Blender \
-	com.bitwig.BitwigStudio \
-	com.github.tchx84.Flatseal \
-	io.github.Omniaevo.mqtt5-explorer	
-
-    # Install supporting packages
-    sudo apt install -y steam-devices
-
-    # Setup permissions
-    flatpak override --user com.usebottles.bottles --filesystem=~/.var/app/com.valvesoftware.Steam/data/Steam
-    flatpak override --user com.usebottles.bottles --talk-name=org.freedesktop.Flatpak 
+        org.jdownloader.JDownloader
 }
 
 upgrade_curl() {
@@ -160,34 +152,45 @@ upgrade_curl() {
     /usr/local/bin/curl --version
 }
 
-
 install_onedrive() {
     echo
-    echo "*** Removing old onedrive installation ..."
+    echo "*** Installing Onedrive client ..."
     echo
-    sudo apt remove -y onedrive
-    sudo add-apt-repository --remove ppa:yann1ck/onedrive
-    sudo rm -f /etc/systemd/user/default.target.wants/onedrive.service
 
     release=`lsb_release -rs 2>/dev/null`
+    repo="https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_$release"
 
-    if [ "$release" == "24.04" ]; then
-    	upgrade_curl
+    grep "$repo" /etc/apt/sources.list.d/onedrive.list >/dev/null 2>&1
+    if [ $? == 0 ]; then
+	echo "Onedrive already installed, skipping!"
+	echo
+    else
+	echo "* Removing old onedrive installations ..."
+	sudo apt remove -y onedrive
+	sudo add-apt-repository --remove ppa:yann1ck/onedrive
+	sudo rm -f /etc/systemd/user/default.target.wants/onedrive.service
+
+	if [ "$release" == "24.04" ]; then
+	    echo
+	    echo "* Installing newer version of curl ..."
+	    echo
+	    upgrade_curl
+	fi
+
+	echo
+	echo "* Installing new onedrive application ..."
+	echo
+	wget -qO - $repo/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] $repo/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
+	sudo apt update
+	sudo apt install --no-install-recommends --no-install-suggests -y onedrive
+
+	echo
+	echo "* Starting onedrive service ..."
+	echo
+	systemctl --user enable onedrive
+	systemctl --user start onedrive
     fi
-
-    echo
-    echo "*** Installing new onedrive application ..."
-    echo
-    wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_$release/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_$release/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
-    sudo apt update
-    sudo apt install --no-install-recommends --no-install-suggests -y onedrive
-
-    echo
-    echo "*** Starting onedrive service ..."
-    echo
-    systemctl --user enable onedrive
-    systemctl --user start onedrive
 }
 
 install_vscode() {
@@ -217,15 +220,191 @@ EOL
     sudo apt install code
 }
 
+install_kvm() {
+    echo
+    echo "*** Installing KVM virtualization ..."
+    echo
+    
+    if [ `egrep -c "(vmx|svm)" /proc/cpuinfo` -gt 0 ]; then
+	echo "KVM capable CPU found .."
+	sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+    else
+	echo "CPU does not support KVM!"
+    fi
+}
+
+install_dev() {
+    echo
+    echo "*** Installing Development IDEs ..."
+    echo
+
+    sudo apt install -y kdevelop clang clang-tidy cppcheck cmake cmake-gui git gitk kdiff3 scons
+
+    sudo flatpak install -y \
+	com.jetbrains.PyCharm-Professional \
+	com.google.AndroidStudio \
+	io.github.Omniaevo.mqtt5-explorer
+}
+
+install_media() {
+    echo
+    echo "*** Installing Media applications ..."
+    echo
+
+    # OBS Studia PPA is not up-to-date with latest Ubuntu version, Flatpak pulls in old dependencies; Install older version from Kubuntu instead
+    #sudo add-apt-repository --yes ppa:obsproject/obs-studio
+    #sudo apt update
+
+    sudo apt install -y obs-studio
+
+    sudo apt install -y kdenlive gimp inkscape scons avidemux-qt
+    sudo apt install -y ardour audacity lmms
+
+    sudo flatpak install -y \
+	org.musescore.MuseScore \
+	com.bitwig.BitwigStudio
+}
+
+install_kicad() {
+    echo
+    echo "*** Installing KiCAD ..."
+    echo
+    sudo add-apt-repository --yes ppa:kicad/kicad-9.0-releases
+    sudo apt update
+    sudo apt install -y --install-recommends kicad
+}
+
+install_cad() {
+    echo
+    echo "*** Installing CAD applications ..."
+    echo
+
+    sudo flatpak install -y \
+        org.freecad.FreeCAD \
+        com.prusa3d.PrusaSlicer \
+	org.blender.Blender
+}
+
+install_games() {
+    echo
+    echo "*** Installing Games and Launchers ..."
+    echo
+
+    sudo flatpak install -y \
+        com.valvesoftware.Steam \
+        com.valvesoftware.SteamLink \
+	com.heroicgameslauncher.hgl \
+	com.usebottles.bottles
+
+    # Install supporting packages
+    sudo apt install -y steam-devices
+
+    # Setup permissions
+    flatpak override --user com.usebottles.bottles --filesystem=~/.var/app/com.valvesoftware.Steam/data/Steam
+    flatpak override --user com.usebottles.bottles --talk-name=org.freedesktop.Flatpak 
+}
+
+install_flightsim() {
+    echo
+    echo "*** Installing Flightsims ..."
+    echo
+
+    sudo flatpak install -y \
+	org.flightgear.FlightGear
+}
+
 help() {
     echo "Usage: ./install.sh [all|grub|onedrive]"
     exit 0
 }
 
+install() {
+    case $1 in
+	system)
+	    install_timezone
+	    install_settings
+	    install_grub
+	    ;;
+	base)
+	    install system
+	    install apps
+	    ;;
+	all)
+	    install base
+
+	    install dev
+	    install cad
+	    install media
+	    install games
+	    ;;
+
+	# Application categories
+	apps)
+	    install_repos
+	    install_packages
+	    install_mozilla_flatpak
+	    install_onedrive
+	    install_apps
+	    ;; 
+	dev)
+	    install_vscode
+	    install_kvm
+	    install_dev
+	    ;;
+	cad)
+	    install_cad
+	    install_kicad
+	    ;;
+	media)
+	    install_media
+	    ;;
+	games)
+	    install_games
+	    install_xtradebs
+	    ;;
+
+	# Additional targets
+	touch)
+	    install_touchscreen
+	    ;;
+	flightsim)
+	    install_flightsim
+	    ;;
+
+	# Individual installers
+	settings)
+	    install_settings
+	    ;;
+	grub)
+	    install_grub
+	    ;;
+	repos)
+	    install_repos
+	    ;;
+	mozilla)
+	    install_mozilla_flatpak
+	    ;;
+	onedrive)
+	    install_onedrive
+	    ;;
+	code)
+	    install_vscode
+	    ;;
+	kicad)
+	    install_kicad
+	    ;;
+	xtradebs)
+	    install_xtradebs
+	    ;;
+	*)
+	    echo "Invalid option: $1"
+	    exit 1
+    esac
+
+}
+
 if [ -z $1 ]; then
-    install_packages
-    install_settings
-    install_timezone
+    install base
     exit 0
 fi
 
@@ -233,46 +412,7 @@ case $1 in
     -h|--help)
 	help
 	;;
-    all)
-	install_packages
-	install_settings
-	install_timezone
-	install_grub
-	install_vscode
-	install_onedrive
-	install_kicad
-	install_apps
-	install_xtradebs
-	install_mozilla_flatpak
-	;;
-    settings)
-	install_settings
-	;;
-    xtradebs)
-	install_xtradebs
-	;;
-    apps)
-	install_apps
-	;;
-    grub)
-	install_grub
-	;;
-    mozilla)
-	install_mozilla_flatpak
-	;;
-    onedrive)
-	install_onedrive
-	;;
-    kicad)
-	install_kicad
-	;;
-    touch)
-	install_touchscreen
-	;;
-    code)
-	install_vscode
-	;;
     *)
-	echo "Invalid option: $1"
-	exit 1
+	install $1
+	;;
 esac
