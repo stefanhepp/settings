84b69e2de1dfc2b0e386b4cf9ffd5a85da720e41
Stefan Hepp <stefan@stefant.org>
2026-03-19 18:36:56 +0100

    Added Epson scanner and qimgv

--- install.sh
+++ install.sh
@@ -44,7 +44,8 @@ install_packages() {
     echo
     echo "*** Installing standard software ..."
     echo
-    sudo apt install -y vim zsh screen ksshaskpass vlc gimp inkscape clang clang-tidy cppcheck cmake cmake-gui git gitk kdiff3 net-tools curl scons
+    sudo apt install -y vim zsh screen ksshaskpass openssh-server vlc qimgv
+    sudo apt install -y clang clang-tidy cppcheck cmake cmake-gui git gitk kdiff3 net-tools curl scons
     sudo apt install -y krusader krename arj rar unrar smb4k
     sudo apt install -y pipx python3-pip python3-serial python3-numpy python3-scipy python3-opencv python3-tk python3-pil.imagetk python3-venv
 }
@@ -141,6 +142,7 @@ install_apps() {
 
     sudo flatpak install -y \
         com.xnview.XnViewMP \
+	net.epson.epsonscan2 \
         com.jgraph.drawio.desktop \
         org.jdownloader.JDownloader
 }
